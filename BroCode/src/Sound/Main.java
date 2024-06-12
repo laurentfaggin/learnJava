d945362defdc4d2659630cddfe8e768cae9e6cbe
laurentfaggin <laurentfaggin@hotmail.fr>
2024-06-12 16:38:01 -0400

    modif Sound

--- BroCode/src/Sound/Main.java
+++ BroCode/src/Sound/Main.java
@@ -1,17 +1,23 @@
 package Sound;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+
 import javax.sound.sampled.*;
 
 public class Main {
 
 	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		Scanner scanner = new Scanner(System.in);
+		
 		File file = new File("/home/laurent/git/repository/BroCode/src/Sound/Skating_on_the_uppers.wav");
 		AudioInputStream audiostream = AudioSystem.getAudioInputStream(file);
 		Clip clip = AudioSystem.getClip();
 		clip.open(audiostream);
 		
 		clip.start();
+		
+		String response = scanner.next();
 	}
 
 }
